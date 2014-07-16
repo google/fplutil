c1e77f60556f141a85a58566dc27d639ecd45fe2
Stewart Miles <smiles@google.com>
2014-07-16 14:19:18 -0700

    Fixed invalid reference to native_app_glue from libfplutil.
    Came across this when trying to import libfplutil from another NDK module.
    
    Tested:
    Verified that it's possible to build a very simple application (hello world)
    that links against libfplutil on Android using ndk-build.
    
    Change-Id: I9620f5c1335ac9bc17ed812cdbeacbbc0b13862d

--- libfplutil/jni/Android.mk
+++ libfplutil/jni/Android.mk
@@ -50,4 +50,4 @@ LOCAL_ARM_MODE:=arm
 LOCAL_STATIC_LIBRARIES:=android_native_app_glue
 include $(BUILD_STATIC_LIBRARY)
 
-$(call import-module, native_app_glue)
+$(call import-module,android/native_app_glue)
