e4931ba5d771387aa78ddff7a97b9d1e9c0894e2
hakuro <hakuro@google.com>
2017-01-06 21:11:05 +0000

    Add Android build support for hyphenation patterns.
    Tested: on OSX, built for Android.
    PiperOrigin-RevId: 143805900
    Change-Id: I9b8e8258280261603a0b055a53f2397410ab3fe7

--- buildutil/android_common.mk
+++ buildutil/android_common.mk
@@ -111,6 +111,10 @@ $(call fplutil_set_to_first_path_that_exists,DEPENDENCIES_HARFBUZZ_DIR,\
 $(call fplutil_set_to_first_path_that_exists,DEPENDENCIES_LIBUNIBREAK_DIR,\
     $(THIRD_PARTY_ROOT)/libunibreak)
 
+# Location of the hyphenation_pattern files.
+$(call fplutil_set_to_first_path_that_exists,DEPENDENCIES_HYPHENATION_PATTERN_DIR,\
+    $(THIRD_PARTY_ROOT)/hyphenation_patterns)
+
 # Location of the Cardboard java library (required for fplbase)
 # TODO(jsanmiya): Temporarily put the cardboard libs in fplutil/libs in case
 # we don't have the ability to grab the Cardboard SDK.
