6d3fed96dd07cba9972da276fcf8b25933e823fe
amaurice <amaurice@google.com>
2017-03-21 15:20:01 -0700

    Removing an extra line in fplutil build file.
    PiperOrigin-RevId: 150810167
    Change-Id: I7ed9c91ad53bc9f5786615d0fa66f3ea767c5959

--- buildutil/android_common.mk
+++ buildutil/android_common.mk
@@ -270,7 +270,6 @@ $(call fplutil_set_to_first_path_that_exists,DEPENDENCIES_GTEST_JNI_DIR,\
 # googletest/googletest.
 $(call fplutil_set_to_first_path_that_exists,DEPENDENCIES_GTEST_DIR,\
     ${THIRD_PARTY_ROOT}/googletest/googletest \
-
     ${THIRD_PARTY_ROOT}/gtest)
 
 # Location of the googlemock library.
